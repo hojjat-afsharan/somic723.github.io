894f141f09a035dade4841dee3ec95c2ba4fc64f
somic723 <afsharan.h@gmail.com>
2018-12-18 21:06:43 +0330

    jQuery Added

---
+++ jQuery/app.js
@@ -0,0 +1,14 @@
+const circle_div = document.querySelector("circle");
+const square_div = document.getElementById("square");
+const a = 20;
+
+$(".circle").click(function(){
+    $(".circle").css("width", "400px");
+})
+
+$("#square").hover(() => console.log("Hello!"));
+
+
+
+// $("div").html("Hi");
+// alert($("div").html());
